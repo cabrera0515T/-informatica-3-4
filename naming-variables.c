2df96bcf525a49d64cda44f50d7c9c32a438d1cb
cabrera0515T <Cabrera_0515@icloud.com>
2026-04-21 15:02:38 +0000

    Variables created successfully

---
+++ naming-variables.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int number; //1 change the ! after the number
+int test_value; // 1 put the _ between test and value
+int example1; // 1 change the one to the end
+float int_value; // 1 added_value next to int
+printf("Variables created successfully.\n");
+}
--- recipe.c
+++ recipe.c
@@ -3,4 +3,6 @@
 int main(void) {
     printf("coconut Cranberry Dessert\n");
 printf("\n");
-}
+printf("1 1/2 cups gragam cracker crumbs\n");
+printf("1/2 cup butter or margarine, melted\n");
+)
