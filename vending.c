b3b9ecbd7c14ec2ef06c4829da17d43d31047c3b
cabrera0515T <Cabrera_0515@icloud.com>
2026-04-21 15:48:28 +0000

    vending

---
+++ vending.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void){
+    //varoables for tortas chuma
+    float pricel=74.99;
+    int quantityl=15;
+    char select1 = "t";
+
+    //variables for tacos Rosas
+    float price2 =109.90;
+    int quantity2 =10;
+    char select2 = "r";
+
+    //variables for drinks
+    float price3 = 5.00
+    int quantity =2,5
+
+}
