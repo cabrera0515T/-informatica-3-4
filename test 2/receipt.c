e0d06956983ab38d899c5d62fa35bc13349a6bb5
cabrera0515T <Cabrera_0515@icloud.com>
2026-04-29 14:56:12 +0000

    test

---
+++ test 2/receipt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void)
+{
+     // Apple
+     float price= $2.00
+     int Quantity= 2
+     char selected= "A"
+
+     // drinks
+     float price2= $1.50
+     int quantity2= 2
+     char selected2= "d"
+     printf("test recipe\n");
+     printf("item name; drinks\n");
+     printf("Total of item\n");
+
+     printf("welcome to receipt.c\n");
+     Welcome to the Snack Shop!
+---------------------------
+Item            Price   Quantity  Code
+Apple          $2.00      2    A
+drinks         $1.50  1       d
+Candy           $0.75   30        C
+
+
+}
